9b13bd4f81d625d1444fc40e9bd4e052f730a2b8
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2024-04-23 15:35:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    const sessionID = Math.floor(Math.random() * (876 - 123 + 1)) + 123;
+    console.log("Guest session ID:", sessionID);
+    
+    const sessionIDElement = document.querySelector('#sessionID');
+    
+    if (sessionIDElement) {
+      // Temporarily stop the typewriter effect on the sessionIDElement
+      stopTypewriter(sessionIDElement);
+      
+      // Set the content of the sessionIDElement
+      sessionIDElement.textContent = sessionID;
+      
+      // Resume the typewriter effect on the sessionIDElement
+      setTimeout(function() {
+        startTypewriter(sessionIDElement);
+      }, 100); // Delay to allow time for setting content
+    } 
+  }, 100); // 100 milliseconds timeout
+});
+
+// Function to stop the typewriter effect on a specific element
+function stopTypewriter(element) {
+  $(element).typewriter('destroy');
+}
+
+// Function to start the typewriter effect on a specific element
+function startTypewriter(element) {
+  $(element).typewriter();
+}
+
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
+const aboutContent = document.querySelector('#about-content');
+const contactContent = document.querySelector('#contact-content');
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    //modal: true,
+    width: '500px',
+    height: '600px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00');
+    },
+    onblur: function () {
+      this.setBackground('#777');
+    },
+  });
+});
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    background: '#00aa00',
+    width: '400px',
+    height: '400px',
+    top: 80,
+    right: 50,
+    bottom: 50,
+    left: 75,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00');
+    },
+    onblur: function () {
+      this.setBackground('#777');
+    },
+  });
+});
+
+// Typewriter.js
+
+$.fn.typewriter = function() {
+  this.each(function() {
+    var c = $(this),
+      b = c.html(),
+      a = 0,
+      d = 0;
+    c.html('');
+    var e = function() {
+      if ('<' == b.substring(a, a + 1)) {
+        var f = new RegExp(/<span class="instant"/),
+          g = new RegExp(/<span class="clear"/);
+        if (b.substring(a, b.length).match(f)) a += b.substring(a, b.length).indexOf('</span>') + 7;
+        else if (b.substring(a, b.length).match(g)) d = a, a += b.substring(a, b.length).indexOf('</span>') + 7;
+        else
+          for (; '>' != b.substring(a, a + 1);) a++;
+      }
+      c.html(b.substring(d, a++) + (a & 1 ? '_' : ''));
+      a >= b.length || setTimeout(e, 0.001);
+    };
+    e();
+  });
+  return this;
+};
+$('.terminal').typewriter();
